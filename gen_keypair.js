5bfb27e070c6df9b2c7345ffdc0a35220bc7c1ba
Jared Wasinger <j-wasinger@hotmail.com>
2023-02-07 21:43:45 -0500

    use sha256

--- gen_keypair.js
+++ gen_keypair.js
@@ -5,15 +5,14 @@ var jwt = require('jsonwebtoken');
 const text = ''
 const key = 'abcdefg'
 
-let secret = crypto.createHmac('sha1', key)
+let secret = crypto.createHmac('sha256', key)
   .update(text)
     .digest('hex')
 
 fs.writeFileSync('jwt_secret', secret);
 
 var token = jwt.sign({}, secret, {
-    algorithm: 'HS256', //algo used to create JWT
-    expiresIn: "2d" // set a 2 day expiration
+    algorithm: 'HS256'
 }, (err, token) => {
     if (err) {
         console.log(err)

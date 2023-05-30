98119b4f11f6b9c9618eb2aaa0a0022dd838db94
Jared Wasinger <j-wasinger@hotmail.com>
2023-05-30 16:48:28 +0200

    wip

--- curl_example.sh
+++ curl_example.sh
@@ -1,6 +1,5 @@
 #! /usr/bin/env bash
 
 TOKEN=$($(pwd)/jwt-go-example/jwt-go-example)
-echo $TOKEN
 
-curl -H 'Content-Type: application/json' -H "Authorization: Bearer $TOKEN" -X POST --data '{"jsonrpc":"2.0","method":"engine_exchangeCapabilities","params":[],"id":1}' http://localhost:8551
+curl -H 'Content-Type: application/json' -H "Authorization: Bearer $TOKEN" -X POST --data '{"jsonrpc":"2.0","method":"engine_exchangeCapabilities","params":[["engine_newPayload"]],"id":1}' http://localhost:8551
